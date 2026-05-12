286fba2f63f07f399bcaffdbcf88ede47c40ad18
Xuân Đại <daik5xuanvinh@gmail.com>
2026-05-12 16:34:29 +0700

    update

---
+++ src/Exercise5/Customer.java
@@ -0,0 +1,10 @@
+package Exercise5;
+
+public class Customer {
+    public int id;
+    public String name;
+    public String email;
+
+    public void showInfo() {
+    }
+}
---
+++ src/Exercise5/Order.java
@@ -0,0 +1,10 @@
+package Exercise5;
+
+public class Order {
+    public int orderId;
+    public Customer customer;
+    public double total;
+
+    public void printOrder() {
+    }
+}
