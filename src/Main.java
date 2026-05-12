b879c60f0824936cc91867766f335a65a3c4ba3d
Xuân Đại <daik5xuanvinh@gmail.com>
2026-05-12 16:35:00 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,2 @@
 public class Main {
-    private String title;
-    private String author;
-    private double price;
-
-    // Method
-    public void printInfo() {
-        System.out.println("Title: " + title);
-        System.out.println("Author: " + author);
-        System.out.println("Price: " + price);
-    }
 }
